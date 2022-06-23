50298bc0f20b6fea277d301792d4acd69b580de0
Jennifer184 <24310135+Jennifer184@users.noreply.github.com>
2022-06-22 23:11:19 -0500

    Update __init__.py

--- weatherApp/weatherApp/__init__.py
+++ weatherApp/weatherApp/__init__.py
@@ -6,4 +6,4 @@ A weather application that reads out loud the temperature.
 
 __version__ = "0.1.0"
 __author__ = 'Jennifer184'
-__credits__ = 'colorado State University'
+__credits__ = 'Colorado State University'
